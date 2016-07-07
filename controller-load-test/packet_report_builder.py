a3c54f03d90efbe20c9d1ba35cf83aca8c2be524
sunshiny92 <1119514881@qq.com>
2016-07-07 18:09:01 +0800

    multi-thread

--- controller-load-test/packet_report_builder.py
+++ controller-load-test/packet_report_builder.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 
 class packet_report_builder:
 
@@ -18,8 +19,30 @@ class packet_report_builder:
     def close(self):
         self._sock.close()
 
-prb = packet_report_builder('127.0.0.1', 1919)
-src = bytearray((10, 0, 0, 1))
-dst = bytearray((10, 0, 0, 1))
-prb.send(src, dst)
-prb.close()
+
+def request_thread(i):
+    for x in range(10*i, 10*i+10):
+        src = bytearray((10, 1, x, 1))
+        for y in range(100):
+            prb = packet_report_builder('127.0.0.1', 1919)
+            dst = bytearray((10, 2, y, 1))
+            prb.send(src, dst)
+            prb.close()
+
+start = time.time()
+tl = []
+
+for i in range(5):
+    t = threading.Thread(target=request_thread, args=(i,))
+    tl.append(t)
+
+for t in tl:
+    t.start()
+
+print "here"
+for t in tl:
+    t.join()
+
+end = time.time()
+
+print round(end - start, 2)
